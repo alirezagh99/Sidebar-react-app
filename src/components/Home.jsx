ca490e9ba6334699319adf53b98c8d4e4a03c450
Alireza Ghasemi <agh102724@gmail.com>
2024-12-30 17:44:17 +0330

    add-Modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,10 @@ function Home() {
       <button onClick={() => ctx.handleOpenSidebar()} className="absolute">
         <IoMenu className="text-3xl text-blue-400 hover:text-blue-800 transition-all duration-300 m-5" />
       </button>
-      <button className="bg-blue-400 hover:bg-blue-800 transition-all duration-300 text-white px-3 py-1 font-medium rounded text-[12px] ml-[50%] mt-[30vh] -translate-x-1/2">
+      <button
+        onClick={() => ctx.handleOpenModal()}
+        className="bg-blue-400 hover:bg-blue-800 transition-all duration-300 text-white px-3 py-1 font-medium rounded text-[12px] ml-[50%] mt-[30vh] -translate-x-1/2"
+      >
         Show Modal
       </button>
     </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { IoClose } from "react-icons/io5";
+import { sidebarContext } from "../store/SidebarCtx";
 function Modal() {
-  return <div>Modal</div>;
+  const ctx = useContext(sidebarContext);
+  return (
+    <>
+      <div
+        onClick={() => ctx.handleCloseModal()}
+        className="absolute z-10 left-0 top-0 right-0 bottom-0 bg-[rgba(0,0,0,0.3)]"
+      ></div>
+      <div className="flex flex-col justify-center p-5 w-1/3 h-1/3 left-1/2 -translate-x-1/2 text-2xl text-center bg-white z-10 absolute">
+        <button
+          onClick={() => ctx.handleCloseModal()}
+          className="absolute right-2 top-2 hover:text-blue-400 transition-all duration-300"
+        >
+          <IoClose />
+        </button>
+        <h3 className="">Modal Content</h3>
+      </div>
+      ;
+    </>
+  );
 }
 
 export default Modal;
